858f9fe2659fb8fe0a2feaf6a99354e49d8ef0bb
jubayer <mohammadjubayer999@gmail.com>
2020-01-28 18:52:20 +0000

    initial commit

--- Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment2.java
+++ Application/src/main/java/com/example/android/camera2basic/Camera2BasicFragment2.java
@@ -1,13 +1,12 @@
 package com.example.android.camera2basic;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
